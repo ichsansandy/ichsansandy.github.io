153de0912078c9683224f9d53c2f75986166c726
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-04-15 17:41:00 +0700

    add animation blob

--- app.js
+++ app.js
@@ -19,3 +19,17 @@ var typed = new Typed(".auto-type", {
     backSpeed: 100,
     loop: true,
   });
+
+const tween = KUTE.fromTo("#blob1",
+  {path:"#blob1"},
+  {path:"#blob2"},
+  {repeat:999, duration:3000, yoyo:true}
+)
+const tween2 = KUTE.fromTo("#blobshadow1",
+  {path:"#blobshadow1"},
+  {path:"#blobshadow2"},
+  {repeat:999, duration:3000, yoyo:true}
+)
+
+tween.start()
+tween2.start()
