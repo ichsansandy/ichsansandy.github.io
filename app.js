82b5f746cf5fa7ec31fdd8dc1213b58ade5ea419
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-04-16 06:35:25 +0700

    add some animation while idle and hover to the social media icon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((element) => {
-    console.log(element);
     if (element.isIntersecting) {
       element.target.classList.add("show");
     } else {
