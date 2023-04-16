41b06329ad80e82af5007493c49abedadddac12d
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-04-16 11:55:53 +0700

    add mouse trail inspired from hyperflexed poppr.be

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((element) => {
-    if (element.isIntersecting) {
-      element.target.classList.add("show");
-    } else {
-      element.target.classList.remove("show");
-    }
-  });
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((element) => {
+//     if (element.isIntersecting) {
+//       element.target.classList.add("show");
+//     } else {
+//       element.target.classList.remove("show");
+//     }
+//   });
+// });
+
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((element) => observer.observe(element));
+
+//typing animation
+var typed = new Typed(".auto-type", {
+  strings: ["Software Engineer", "Full Stack Developer", "Back-End Engineer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((element) => observer.observe(element));
+// blob animation
+const tween = KUTE.fromTo("#blob1", { path: "#blob1" }, { path: "#blob2" }, { repeat: 999, duration: 3000, yoyo: true });
+const tween2 = KUTE.fromTo("#blobshadow1", { path: "#blobshadow1" }, { path: "#blobshadow2" }, { repeat: 999, duration: 3000, yoyo: true });
 
+tween.start();
+tween2.start();
 
-var typed = new Typed(".auto-type", {
-    strings: ["Software Engineer", "Full Stack Developer", "Back-End Engineer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-  });
+// mouse animation
+const magiclight = document.querySelector("#magiclight-blurry");
 
-const tween = KUTE.fromTo("#blob1",
-  {path:"#blob1"},
-  {path:"#blob2"},
-  {repeat:999, duration:3000, yoyo:true}
-)
-const tween2 = KUTE.fromTo("#blobshadow1",
-  {path:"#blobshadow1"},
-  {path:"#blobshadow2"},
-  {repeat:999, duration:3000, yoyo:true}
-)
-
-tween.start()
-tween2.start()
+document.body.onpointermove = (event) => {
+  const { x, y } = event;
+  magiclight.animate({
+    left : `${x}px`,
+    top : `${y}px`
+  }, {duration:3000, fill:"forwards"})
+};
